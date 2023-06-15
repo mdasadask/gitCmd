581efbd88d21342f3cbef81b36cdaf07338311f0
Asad <mdasadask@gmail.com>
2023-06-15 22:12:51 +0600

    add all command at first

---
+++ git.php
@@ -0,0 +1,45 @@
+1. vim new.txt (vim editor open hobe. jokhon niche insert lekha .thake tokhon lekha jay. i enter korle insert ase.
+lekha ses hole esc dile niche abar command deya jay. :x/:wq command likhe enter
+dile save o exit hoy.)
+
+2. ls (show file)
+3. ls -al (show all file hidden)
+4. git show abcdef
+5. git checkout abcdef
+6. git checkout master
+7. git checkout master -f
+8. git remote add origin link (origin remote)
+9. git remote show
+10. git remote show origin
+11. git push origin master
+12. git pull origin master
+13. touch asad.php
+
+14. git config --global user.name "Asad80"
+15. git config --global user.email "mdasad_ask@yahoo.com"
+16. git config credential.username "Asad80"
+17. git config --global -e
+18. git config --list / git config -l
+
+-----------------------------------------------------------------------------------------------------------------------
+New learned git command
+
+14. ### Ekbar commit korar por command gulo kaj kore ###
+git add -A/--all sob file stage tule dey.
+R git add . sudhu je folder ace sei folder er sob stage tule dey.
+git add * deleted file gulo chara baki gulo stage tule dey.
+15. (cd ../ == cd..) dile ek step back a chole jay.
+16. HEAD^ (^ = caret(ক্যারেট)/exponent)
+git reset --soft HEAD^ >> current commit remove hoye, tar ager commit current commit hoy. Ebong current commit stagging area te chole jay
+git reset --hard HEAD^ >> current commit remove hoye, tar ager commit current commit hoy. Ebong current commit R paouya jay na.
+git reset HEAD^ >> current commit remove hoye, tar ager commit current commit hoy. Ebong current commit working area te chole jay
+
+git reset dile reset hobe
+git reset --hard dile deleted file gulo soho chole ase.
+
+17. git restore fileName
+18. git rm --cached fileName (unstage file)
+git rm -r --cached folderName (unstage folder) [-r = recursive(পুনরাবৃত্তি)]
+19. git diff
+20. git log
+21. git log --oneline
